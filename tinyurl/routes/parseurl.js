4d41ec287838280bf2ba4c546a179e6909350dd6
Umang Suthar <umangsuthar@localhost.localdomain>
2017-06-30 16:47:20 +0530

    Final Insertion and shorted done

--- tinyurl/routes/parseurl.js
+++ tinyurl/routes/parseurl.js
@@ -10,30 +10,42 @@ router.post('/', function(req, res, next) {
     var shorturl = '';
     
     var index;
-   
-    db.collection('urls').find({},{_id:0,index:1}).sort({index:-1}).limit(1).toArray(function(err,item){
-        
-        index = item[0].index + 1;
-        console.log(index);
-    
-   
-    var temp = index;
-    //url shortner
-    
-    while(index > 0)
-    {
-        shorturl += map[index%62];
-        index = Math.round(index/62);
-    }
-    
-    db.collection('urls').insert({fullUrl:url1,index:temp,shortUrl:shorturl});
-   
-   
-   
-    console.log(shorturl);
-    res.send("http://localhost:3000/"+shorturl);
-   });
+       db.collection('urls').find({fullUrl:url1}).toArray(function(err,item){
+                if ( item.length > 0 )     {
+                    console.log("already short");
+                    console.log(item);
+                    res.send("http://localhost:3000/"+item[0].shortUrl);
+                }
+           else{
+                console.log("NOT NOT found");
+          
+                    db.collection('urls').find({},{_id:0,index:1}).sort({index:-1}).limit(1).toArray(function(err,item){
 
+                                    index = item[0].index + 1;
+                                    console.log(index);
+
+
+                                var temp = index;
+                                //url shortner
+
+                                while(index > 0)
+                                {
+                                    shorturl += map[index%62];
+                                    index = Math.round(index/62);
+                                }
+
+                                db.collection('urls').insert({fullUrl:url1,index:temp,shortUrl:shorturl});
+
+
+
+                                console.log(shorturl);
+                                res.send("http://localhost:3000/"+shorturl);
+                       });
+            
+               
+                }
+       });
+               
 });
     
 module.exports = router;
